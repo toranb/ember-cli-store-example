b8c88714855f0651a973b02ad36e5122e9b41d55
toran billups <toranb@gmail.com>
2015-03-23 18:29:43 -0500

    [DEPS]: updated to ember-cli 0.2.0 and ember 1.11 beta 5

--- app/routes/people.js
+++ app/routes/people.js
@@ -4,12 +4,12 @@ import PromiseMixin from 'ember-promise/mixins/promise';
 var PeopleRoute = Ember.Route.extend({
     model: function() {
         var store = this.get("store");
-        PromiseMixin.xhr("/api/people", "GET").then(function(response) {
+        return PromiseMixin.xhr("/api/people", "GET").then(function(response) {
             response.forEach(function(data) {
                 store.push("person", data);
             });
+            return store.find("person");
         });
-        return store.find("person");
     }
 });
 
--- tests/acceptance/acceptance-test.js
+++ tests/acceptance/acceptance-test.js
@@ -1,11 +1,15 @@
 import Ember from "ember";
-import { test } from 'ember-qunit';
+import {test, module} from 'qunit';
 import startApp from '../helpers/start-app';
 
-var App;
+var App, people = [{id: 1, firstName: 'toran', lastName: 'billups'}, {id: 2, firstName: 'brandon', lastName: 'williams'}];
 
 module('Example Acceptance Test', {
     setup: function() {
+        $.fauxjax.new({
+          url: '/api/people',
+          responseText: people
+        });
         App = startApp();
     },
     teardown: function() {
@@ -13,16 +17,14 @@ module('Example Acceptance Test', {
     }
 });
 
-test("people route will resolve promise and bind ember objects", function() {
-    var people = [{id: 1, firstName: 'toran', lastName: 'billups'}, {id: 2, firstName: 'brandon', lastName: 'williams'}];
-    $.fauxjax.new({type: "GET", url: "/api/people", dataType: 'json', responseText: people});
+test("people route will resolve promise and bind ember objects", function(assert) {
     visit("/");
     andThen(function() {
         var rows = find(".name");
-        equal(rows.length, 2);
+        assert.equal(rows.length, 2);
         var first = find(".name:eq(0)").text();
-        equal(first, "toran");
+        assert.equal(first, "toran");
         var last = find(".name:eq(1)").text();
-        equal(last, "brandon");
+        assert.equal(last, "brandon");
     });
 });
